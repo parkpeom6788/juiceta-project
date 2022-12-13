d1eb6900da8ea4448ed9be1176cc7e745408c08a
ParkJinSoo189 <xnqlwlrh@naver.com>
2022-12-13 17:19:05 +0900

    박진수- CustomerVO,OrderVO,ReviewVO 생성

--- juiceta-project/src/main/java/org/kosta/juicetaproject/model/vo/CustomerVO.java
+++ juiceta-project/src/main/java/org/kosta/juicetaproject/model/vo/CustomerVO.java
@@ -1,5 +1,14 @@
 package org.kosta.juicetaproject.model.vo;
 
-public class CustomerVO {
+import java.io.Serializable;
 
+public class CustomerVO implements Serializable{
+	private static final long serialVersionUID = 2369600641845334659L;
+	private String id;
+	private String password;
+	private String name;
+	private String address;
+	private String phone;
+	private int enabled;
+	//private AuthoritiesVO authoritiesVO;
 }
