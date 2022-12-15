609ddb16f4bb2c1a5ab54857423cf080a3b9793a
ParkJinSoo189 <xnqlwlrh@naver.com>
2022-12-15 12:14:15 +0900

    박진수 Customer Insert 재등록

--- juiceta-project/src/test/java/psj.sql
+++ juiceta-project/src/test/java/psj.sql
@@ -1,43 +1,19 @@
-CREATE TABLE juiceta_authorities(
-	authority VARCHAR2(30) PRIMARY KEY
-)
+-- 고객 ( enabled 탈퇴 0, 정상 1 ) 
 CREATE TABLE juiceta_customer(
 	id VARCHAR2(100) PRIMARY KEY,
 	password VARCHAR2(100) NOT NULL,
 	name VARCHAR2(100) NOT NULL,
 	address VARCHAR2(100)  NOT NULL,
 	phone VARCHAR2(100) NOT NULL,
-	enabled NUMBER DEFAULT 1 NOT NULL,
-	authority VARCHAR2(30) NOT NULL,
-	CONSTRAINT fk_authority FOREIGN KEY(authority) REFERENCES juiceta_authorities(authority) ON DELETE CASCADE
+	enabled NUMBER DEFAULT 1 NOT NULL
 )
-
-INSERT INTO juiceta_authorities(authority) VALUES ('회원');
-
-INSERT INTO juiceta_customer (id,password,name,address,phone,authority) 
-VALUES ('javakong','a','자바콩','오리','01012345678','회원')
-COMMIT
--- 전체 고객 조회 SQL
-SELECT * FROM juiceta_customer
--- id password로 회원찾기 SQL
-SELECT id,password,name,address,phone,enabled,a.authority
-FROM juiceta_customer c
-INNER JOIN juiceta_authorities a ON c.authority=a.authority
-WHERE c.id='javakong' AND c.password='a'
+INSERT INTO juiceta_customer(id,password,name,address,phone) VALUES ('spring','a','프링이','오리','01011112222');
+SELECT * FROM juiceta_customer;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+CREATE TABLE juiceta_authorities(
+	authority VARCHAR2(30) PRIMARY KEY,
+	id VARCHAR2(100) NOT NULL,
+	CONSTRAINT fk_authorities_id FOREIGN KEY(id) REFERENCES juiceta_customer(id) ON DELETE CASCADE
+)
+SELECT * FROM juiceta_authorities
