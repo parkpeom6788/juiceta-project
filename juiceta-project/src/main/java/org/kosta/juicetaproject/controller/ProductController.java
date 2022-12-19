e1eff87b01595c60c3360a50cc4ce52fac3fecde
parkpeom6788 <parkpeom6788@gmail.com>
2022-12-19 11:18:58 +0900

    박범-문의사항 작업중

--- juiceta-project/src/main/java/org/kosta/juicetaproject/controller/ProductController.java
+++ juiceta-project/src/main/java/org/kosta/juicetaproject/controller/ProductController.java
@@ -20,26 +20,30 @@ public class ProductController {
 	private final ProductMapper productMapper;
 	private final QuestionService QuestionService;
 	
+	// 상품 상세 페이지로 이동 
 	@RequestMapping("/guest/DetailView")
 	public String productDetail(int productNo,Model model) {
-		System.out.println(productNo);
-		
 		List<QuestionVO> questionAllList= QuestionService.findQuestionAllListByProductNo(productNo);
-		
-		for(int i=0; i< questionAllList.size(); i++) {
-			System.out.println(questionAllList.get(i));
-	}
 		model.addAttribute("questionAllList", questionAllList);
+		model.addAttribute("productNo", productNo);
 		return "product/product-detail";
+	}
+	// 글 작성 
+	@RequestMapping("/guest/registerProductForm")
+	public String registerProductForm() {
+		return "product/register-form";
+	}
+	// 글 상세 페이지
+	//questionDetail(in questionNo:int): QuestionVO
+	@RequestMapping("/guest/Detail")
+	public String questionDetail(int productNo,Model model) {
+		model.addAttribute("productNo", productNo);
+		return "/guest/Detail";
 	}
 	@RequestMapping("productAdmin")
 	public String productAdmin(Model model) {
 		model.addAttribute("productVO",productMapper.findProductAllList());
 		return "product/product-list";
-	}
-	@RequestMapping("registerProductForm")
-	public String registerProductForm() {
-		return "product/register-form";
 	}
 	@RequestMapping("updateProductForm")
 	public String updateProductForm() {
--- juiceta-project/src/main/java/org/kosta/juicetaproject/model/mapper/QuestionMapper.java
+++ juiceta-project/src/main/java/org/kosta/juicetaproject/model/mapper/QuestionMapper.java
@@ -3,13 +3,26 @@ package org.kosta.juicetaproject.model.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.kosta.juicetaproject.model.vo.AnswerVO;
 import org.kosta.juicetaproject.model.vo.QuestionVO;
 
 @Mapper
 public interface QuestionMapper {
-
 	// 등록 Q&A 
 	void registerQuestion(QuestionVO questionVO);
 
+	// 게시물 전체 리스트 조회
 	List<QuestionVO> findQuestionAllListByProductNo(int productNo);
+	
+	// 상세게시물
+	QuestionVO questionDetail(int questionNo);
+	
+	// 관리자 답변 등록
+	void registerAnswer(AnswerVO answerVO);
+
+	// 게시물 작성
+	void registerQuestion(String questionTitle, String questionContent,int productNo,String id);
+
+	// 답변여부 
+	AnswerVO findAnswerByQuestionNo(int questionNo);
 }
