119216ad247f491bf259294397f6a0dfc34e3636
sgus0608 <sgus0608@naver.com>
2022-12-20 11:28:29 +0900

    임상현 - 테스트페이지 오류 제거

--- juiceta-project/src/test/java/org/kosta/juicetaproject/Testlsh.java
+++ juiceta-project/src/test/java/org/kosta/juicetaproject/Testlsh.java
@@ -1,10 +1,7 @@
 package org.kosta.juicetaproject;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.kosta.juicetaproject.model.mapper.BoardMapper;
-import org.kosta.juicetaproject.model.vo.BoardVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -21,40 +18,29 @@ class Testlsh {
 	@Test
 	void contextLoads() {
 	}
-	@Test
-	public void boardlist() {
-		List<BoardVO> list = boardMapper.findBoardAllList();
-		for(BoardVO vo : list)
-			System.out.println(vo);
-	}
-	
-	@Test
-	public void boardUpdate() {
-		BoardVO vo = new BoardVO(1, "수정2", "수정2", null, 0, 0);
-		boardMapper.boardUpdate(vo);
-		List<BoardVO> list = boardMapper.findBoardAllList();
-		for(BoardVO bvo : list)
-			System.out.println(bvo);
-	}
-	@Test
-	public void boardWrite() {
-		String boardTitle = "test";
-		String boardContent = "text"; 
-		BoardVO vo= new BoardVO(0,boardTitle,boardContent,null,0, 0);
-		boardMapper.registerBoard(vo);
-		List<BoardVO> list = (List<BoardVO>) boardMapper.findBoardAllList(null);
-		for(BoardVO bvo : list)
-			System.out.println(bvo);
-	}
-	@Test void deleteBoard() {
-		int No = 1;
-		boardMapper.deleteBoard(No);
-		List<BoardVO> list = boardMapper.findBoardAllList();
-		for(BoardVO bvo : list)
-			System.out.println(bvo);
-	}
+	/*
+	 * @Test public void boardlist() { List<BoardVO> list =
+	 * boardMapper.findBoardAllList(); for(BoardVO vo : list)
+	 * System.out.println(vo); }
+	 * 
+	 * @Test public void boardUpdate() { BoardVO vo = new BoardVO(1, "수정2", "수정2",
+	 * null, 0, 0); boardMapper.boardUpdate(vo); List<BoardVO> list =
+	 * boardMapper.findBoardAllList(); for(BoardVO bvo : list)
+	 * System.out.println(bvo); }
+	 * 
+	 * @Test public void boardWrite() { String boardTitle = "test"; String
+	 * boardContent = "text"; BoardVO vo= new
+	 * BoardVO(0,boardTitle,boardContent,null,0, 0); boardMapper.registerBoard(vo);
+	 * List<BoardVO> list = (List<BoardVO>) boardMapper.findBoardAllList(null);
+	 * for(BoardVO bvo : list) System.out.println(bvo); }
+	 * 
+	 * @Test void deleteBoard() { int No = 1; boardMapper.deleteBoard(No);
+	 * List<BoardVO> list = boardMapper.findBoardAllList(); for(BoardVO bvo : list)
+	 * System.out.println(bvo); }
+	 */
 	
 	@Test void countListBoard() {
 		int countListBoard = boardMapper.getTotalBoardCount();
+		System.out.println(countListBoard);
 	}
 }
