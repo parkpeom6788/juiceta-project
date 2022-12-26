140e82eefca412c0ff84ed792b60f47fd7b1e8ae
ParkJinSoo189 <xnqlwlrh@naver.com>
2022-12-26 17:54:33 +0900

    박진수 - 테스트 주석처리

--- juiceta-project/src/test/java/org/kosta/juicetaproject/Testjsh.java
+++ juiceta-project/src/test/java/org/kosta/juicetaproject/Testjsh.java
@@ -22,268 +22,268 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Testjsh {
-	private final ProductMapper productMapper;
-	private final MemberMapper memberMapper;
-	private final CartAndWishlistMapper cartAndWishlistMapper;
-	private final OrderMapper orderMapper;
-	private final ReviewMapper reviewMapper;
-
-	@Autowired
-	public Testjsh(ProductMapper productMapper, MemberMapper memberMapper, CartAndWishlistMapper cartAndWishlistMapper, OrderMapper orderMapper, ReviewMapper reviewMapper) {
-		super();
-		this.productMapper = productMapper;
-		this.memberMapper = memberMapper;
-		this.cartAndWishlistMapper = cartAndWishlistMapper;
-		this.orderMapper = orderMapper;
-		this.reviewMapper = reviewMapper;
-	}
-
-	@Test
-	void contextLoads() {
-		System.out.println(productMapper);
-	}
-	
-	@Test
-	void findProductAllList() {
-		int totalProductCount = productMapper.getTotalProductCount();
-		String pageNo = "";
-		Pagination pagination = null;
-		
-		if(pageNo=="")
-			pagination = new Pagination(totalProductCount);
-		else
-			pagination = new Pagination(Integer.parseInt(pageNo), totalProductCount);
-
-		List<ProductVO> list = productMapper.findShopProductAllList(pagination);
-		for(ProductVO vo : list)
-			System.out.println(vo);
-	}
-	
-	@Test
-	void findProductAllListByCateory() {
-		int totalProductCount = productMapper.getTotalProductCount();
-		String pageNo = "";
-		Pagination pagination = null;
-		
-		if(pageNo=="")
-			pagination = new Pagination(totalProductCount);
-		else
-			pagination = new Pagination(Integer.parseInt(pageNo), totalProductCount);
-		
-		String category = "tea";
-		Map<String, Object> map = new HashMap<>();
-		map.put("PAGINATION", pagination);
-		map.put("CATEGORY", category);
-		
-		List<ProductVO> list = productMapper.findProductAllListByCategory(map);
-		for(ProductVO vo : list)
-			System.out.println(vo);
-	}
-	
-	@Test
-	void findMemberId() {
-		MemberVO memberVO = new MemberVO();
-		memberVO.setName("java");
-		memberVO.setPhone("01000000000");
-		String id = memberMapper.findMemberId(memberVO);
-		System.out.println(id);
-	}
-	
-	@Test
-	void findMemberPassword() {
-		MemberVO memberVO = new MemberVO();
-		memberVO.setId("java");
-		memberVO.setName("관리자");
-		memberVO.setPhone("01000000000");		
-		int result = memberMapper.findMemberPassword(memberVO);
-		Assertions.assertEquals(1, result);
-	}
-	
-	@Test
-	void findWishlistByIdAndProductNo() {
-		String id = "java";
-		int productNo = 1;
-		Map<String, Object> map = new HashMap<>();
-		map.put("ID",id);
-		map.put("PRODUCT_NO", productNo);
-		int result = cartAndWishlistMapper.findWishlistByIdAndProductNo(map);
-		System.out.println(result);
-	}
-	
-	@Test
-	void findProductByProductNameKeyword() {
-		String keyword = "양";
-		List<ProductVO> list = productMapper.findProductByProductNameKeyword(keyword);
-		for(ProductVO vo : list)
-			System.out.println(vo);
-	}
-	
-	@Test
-	void findCartByIdAndProductNo() {
-		String id = "java";
-		int productNo = 1;
-		Map<String, Object> map = new HashMap<>();
-		map.put("ID", id);
-		map.put("PRODUCT_NO", productNo);
-		int result = cartAndWishlistMapper.findCartByIdAndProductNo(map);
-		Assertions.assertEquals(1, result);
-	}
-	
-	@Test
-	void addCart() {
-		String id = "jtest3";
-		int productNo = 64;
-		int productCount = 1;
-		Map<String, Object> map = new HashMap<>();
-		map.put("ID", id);
-		map.put("PRODUCT_NO", productNo);
-		map.put("PRODUCT_COUNT", productCount);
-		cartAndWishlistMapper.addCart(map);
-		int result = cartAndWishlistMapper.findCartByIdAndProductNo(map);
-		Assertions.assertEquals(1, result);
-	}
-	
-	@Test
-	void getTotalCartById() {
-		String id = "jtest3";
-		int totalCount = cartAndWishlistMapper.getTotalCartById(id);
-		System.out.println(totalCount);
-	}
-	
-	@Test
-	void placeAnOrder() {
-		MemberVO memberVO = new MemberVO();
-		memberVO.setId("jtest3");
-		
-		// 주문하기
-		OrderVO orderVO = OrderVO.builder().receiverName("강하늘").receiverPhone("000000000").receiverAddress("오리").memberVO(memberVO).build();
-		System.out.println("주문 DB저장 전 : "+orderVO);
-		orderMapper.placeAnOrder(orderVO);
-		System.out.println("주문 DB저장 후 : "+orderVO);
-		
-		// 주문상세
-		int productNo = 332;
-		int productCount = 2;
-		int orderNo = orderVO.getOrderNo();
-		Map<String, Integer> map = new HashMap<>();
-		map.put("PRODUCT_NO", productNo);
-		map.put("PRODUCT_COUNT", productCount);
-		map.put("ORDER_NO", orderNo);
-		orderMapper.placeAnOrderDetail(map);
-	}
-	
-	@Test
-	void findOrderByOrderNo() {
-		int productNo = 3;
-		Map<String, String> map = orderMapper.findOrderByOrderNo(productNo);
-		System.out.println(map.get("RECEIVER_NAME"));
-	}
-	
-	@Test
-	void findOrderListById() {
-		String id = "jtest3";
-		List<Map<String, Object>> list = orderMapper.findOrderListById(id);
-		for(Map<String, Object> map : list) {
-			System.out.println(map.get("ORDER_NO"));
-			System.out.println(map.get("ORDER_TIME"));
-			System.out.println(map.get("TOTAL_PRICE"));
-		}
-	}
-	
-	@Test
-	void getTotalOrderById() {
-		String id = "jtest3";
-		int result = orderMapper.getTotalOrderById(id);
-		System.out.println(result);
-	}
-	
-	@Test
-	void findOrderListByIdPagination() {
-		String id= "jtest3";
-		int totalOrder = orderMapper.getTotalOrderById(id);
-		String pageNo = "";
-		Pagination pagination = null;
-		
-		if(pageNo=="")
-			pagination = new Pagination(totalOrder);
-		else
-			pagination = new Pagination(Integer.parseInt(pageNo), totalOrder);
-		
-		Map<String, Object> map = new HashMap<>();
-		map.put("ID", id);
-		map.put("PAGINATION", pagination);
-
-		List<Map<String, Object>> list = orderMapper.findOrderListByIdPagination(map);
-		for(Map<String, Object> m : list) {
-			System.out.println(m.get("ORDER_NO"));
-			System.out.println(m.get("ORDER_TIME"));
-			System.out.println(m.get("TOTAL_PRICE"));
-		}
-	}
-	
-	@Test
-	void registerReview() {
-		MemberVO memberVO = new MemberVO();
-		memberVO.setId("jtest3");
-		OrderVO orderVO = new OrderVO();
-		orderVO.setOrderNo(5);
-		ProductVO productVO = new ProductVO();
-		productVO.setProductNo(326);
-		OrderDetailVO orderDetailVO = new OrderDetailVO();
-		orderDetailVO.setOrderVO(orderVO);
-		orderDetailVO.setProductVO(productVO);
-		
-		ReviewVO reviewVO = ReviewVO.builder().reviewContent("굉장히 맛있어요~^^").star(4).memberVO(memberVO).orderDetailVO(orderDetailVO).build();
-		
-		reviewMapper.registerReview(reviewVO);
-		System.out.println("실행");
-	}
-	
-	@Test
-	void findReviewListByProductNo() {
-		int productNo = 331;
-		List<ReviewVO> list = reviewMapper.findReviewListByProductNo(productNo);
-		for(ReviewVO vo : list)
-			System.out.println(vo);
-	}
-	
-	@Test
-	void findOrderInfoForReviewByOrderNoAndProductNo() {
-		int orderNo = 1;
-		int productNo = 332;
-		
-		Map<String, String> map = new HashMap<>();
-		map.put("PRODUCT_NAME", reviewMapper.findProductNameForReviewByProductNo(productNo));
-		map.put("ORDER_TIME", reviewMapper.findOrderTimeForReviewByOrderNo(orderNo));
-		
-		System.out.println(map.get("PRODUCT_NAME"));
-		System.out.println(map.get("ORDER_TIME"));
-	}
-
-	@Test
-	void avgStarByProductNo() {
-		int productNo = 331;
-		int avgStar = 0;
-		List<ReviewVO> list = reviewMapper.findReviewListByProductNo(productNo);
-		if(list.size()!=0) {
-			avgStar = reviewMapper.avgStarByProductNo(productNo);
-		}
-		System.out.println(avgStar);
-	}
-	
-	@Test
-	void countProductInCartById() {
-		String id = "spring1";
-		int totalCount = cartAndWishlistMapper.countCartById(id);
-		System.out.println(totalCount);
-	}
-	
-	@Test
-	void getAllReviewList() {
-		String id = "spring1";
-		List<ReviewVO> list = reviewMapper.getAllReviewList(id);
-		for(ReviewVO vo : list)
-			System.out.println(vo);
-	}
+//	private final ProductMapper productMapper;
+//	private final MemberMapper memberMapper;
+//	private final CartAndWishlistMapper cartAndWishlistMapper;
+//	private final OrderMapper orderMapper;
+//	private final ReviewMapper reviewMapper;
+//
+//	@Autowired
+//	public Testjsh(ProductMapper productMapper, MemberMapper memberMapper, CartAndWishlistMapper cartAndWishlistMapper, OrderMapper orderMapper, ReviewMapper reviewMapper) {
+//		super();
+//		this.productMapper = productMapper;
+//		this.memberMapper = memberMapper;
+//		this.cartAndWishlistMapper = cartAndWishlistMapper;
+//		this.orderMapper = orderMapper;
+//		this.reviewMapper = reviewMapper;
+//	}
+//
+//	@Test
+//	void contextLoads() {
+//		System.out.println(productMapper);
+//	}
+//	
+//	@Test
+//	void findProductAllList() {
+//		int totalProductCount = productMapper.getTotalProductCount();
+//		String pageNo = "";
+//		Pagination pagination = null;
+//		
+//		if(pageNo=="")
+//			pagination = new Pagination(totalProductCount);
+//		else
+//			pagination = new Pagination(Integer.parseInt(pageNo), totalProductCount);
+//
+//		List<ProductVO> list = productMapper.findShopProductAllList(pagination);
+//		for(ProductVO vo : list)
+//			System.out.println(vo);
+//	}
+//	
+//	@Test
+//	void findProductAllListByCateory() {
+//		int totalProductCount = productMapper.getTotalProductCount();
+//		String pageNo = "";
+//		Pagination pagination = null;
+//		
+//		if(pageNo=="")
+//			pagination = new Pagination(totalProductCount);
+//		else
+//			pagination = new Pagination(Integer.parseInt(pageNo), totalProductCount);
+//		
+//		String category = "tea";
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("PAGINATION", pagination);
+//		map.put("CATEGORY", category);
+//		
+//		List<ProductVO> list = productMapper.findProductAllListByCategory(map);
+//		for(ProductVO vo : list)
+//			System.out.println(vo);
+//	}
+//	
+//	@Test
+//	void findMemberId() {
+//		MemberVO memberVO = new MemberVO();
+//		memberVO.setName("java");
+//		memberVO.setPhone("01000000000");
+//		String id = memberMapper.findMemberId(memberVO);
+//		System.out.println(id);
+//	}
+//	
+//	@Test
+//	void findMemberPassword() {
+//		MemberVO memberVO = new MemberVO();
+//		memberVO.setId("java");
+//		memberVO.setName("관리자");
+//		memberVO.setPhone("01000000000");		
+//		int result = memberMapper.findMemberPassword(memberVO);
+//		Assertions.assertEquals(1, result);
+//	}
+//	
+//	@Test
+//	void findWishlistByIdAndProductNo() {
+//		String id = "java";
+//		int productNo = 1;
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("ID",id);
+//		map.put("PRODUCT_NO", productNo);
+//		int result = cartAndWishlistMapper.findWishlistByIdAndProductNo(map);
+//		System.out.println(result);
+//	}
+//	
+//	@Test
+//	void findProductByProductNameKeyword() {
+//		String keyword = "양";
+//		List<ProductVO> list = productMapper.findProductByProductNameKeyword(keyword);
+//		for(ProductVO vo : list)
+//			System.out.println(vo);
+//	}
+//	
+//	@Test
+//	void findCartByIdAndProductNo() {
+//		String id = "java";
+//		int productNo = 1;
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("ID", id);
+//		map.put("PRODUCT_NO", productNo);
+//		int result = cartAndWishlistMapper.findCartByIdAndProductNo(map);
+//		Assertions.assertEquals(1, result);
+//	}
+//	
+//	@Test
+//	void addCart() {
+//		String id = "jtest3";
+//		int productNo = 64;
+//		int productCount = 1;
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("ID", id);
+//		map.put("PRODUCT_NO", productNo);
+//		map.put("PRODUCT_COUNT", productCount);
+//		cartAndWishlistMapper.addCart(map);
+//		int result = cartAndWishlistMapper.findCartByIdAndProductNo(map);
+//		Assertions.assertEquals(1, result);
+//	}
+//	
+//	@Test
+//	void getTotalCartById() {
+//		String id = "jtest3";
+//		int totalCount = cartAndWishlistMapper.getTotalCartById(id);
+//		System.out.println(totalCount);
+//	}
+//	
+//	@Test
+//	void placeAnOrder() {
+//		MemberVO memberVO = new MemberVO();
+//		memberVO.setId("jtest3");
+//		
+//		// 주문하기
+//		OrderVO orderVO = OrderVO.builder().receiverName("강하늘").receiverPhone("000000000").receiverAddress("오리").memberVO(memberVO).build();
+//		System.out.println("주문 DB저장 전 : "+orderVO);
+//		orderMapper.placeAnOrder(orderVO);
+//		System.out.println("주문 DB저장 후 : "+orderVO);
+//		
+//		// 주문상세
+//		int productNo = 332;
+//		int productCount = 2;
+//		int orderNo = orderVO.getOrderNo();
+//		Map<String, Integer> map = new HashMap<>();
+//		map.put("PRODUCT_NO", productNo);
+//		map.put("PRODUCT_COUNT", productCount);
+//		map.put("ORDER_NO", orderNo);
+//		orderMapper.placeAnOrderDetail(map);
+//	}
+//	
+//	@Test
+//	void findOrderByOrderNo() {
+//		int productNo = 3;
+//		Map<String, String> map = orderMapper.findOrderByOrderNo(productNo);
+//		System.out.println(map.get("RECEIVER_NAME"));
+//	}
+//	
+//	@Test
+//	void findOrderListById() {
+//		String id = "jtest3";
+//		List<Map<String, Object>> list = orderMapper.findOrderListById(id);
+//		for(Map<String, Object> map : list) {
+//			System.out.println(map.get("ORDER_NO"));
+//			System.out.println(map.get("ORDER_TIME"));
+//			System.out.println(map.get("TOTAL_PRICE"));
+//		}
+//	}
+//	
+//	@Test
+//	void getTotalOrderById() {
+//		String id = "jtest3";
+//		int result = orderMapper.getTotalOrderById(id);
+//		System.out.println(result);
+//	}
+//	
+//	@Test
+//	void findOrderListByIdPagination() {
+//		String id= "jtest3";
+//		int totalOrder = orderMapper.getTotalOrderById(id);
+//		String pageNo = "";
+//		Pagination pagination = null;
+//		
+//		if(pageNo=="")
+//			pagination = new Pagination(totalOrder);
+//		else
+//			pagination = new Pagination(Integer.parseInt(pageNo), totalOrder);
+//		
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("ID", id);
+//		map.put("PAGINATION", pagination);
+//
+//		List<Map<String, Object>> list = orderMapper.findOrderListByIdPagination(map);
+//		for(Map<String, Object> m : list) {
+//			System.out.println(m.get("ORDER_NO"));
+//			System.out.println(m.get("ORDER_TIME"));
+//			System.out.println(m.get("TOTAL_PRICE"));
+//		}
+//	}
+//	
+//	@Test
+//	void registerReview() {
+//		MemberVO memberVO = new MemberVO();
+//		memberVO.setId("jtest3");
+//		OrderVO orderVO = new OrderVO();
+//		orderVO.setOrderNo(5);
+//		ProductVO productVO = new ProductVO();
+//		productVO.setProductNo(326);
+//		OrderDetailVO orderDetailVO = new OrderDetailVO();
+//		orderDetailVO.setOrderVO(orderVO);
+//		orderDetailVO.setProductVO(productVO);
+//		
+//		ReviewVO reviewVO = ReviewVO.builder().reviewContent("굉장히 맛있어요~^^").star(4).memberVO(memberVO).orderDetailVO(orderDetailVO).build();
+//		
+//		reviewMapper.registerReview(reviewVO);
+//		System.out.println("실행");
+//	}
+//	
+//	@Test
+//	void findReviewListByProductNo() {
+//		int productNo = 331;
+//		List<ReviewVO> list = reviewMapper.findReviewListByProductNo(productNo);
+//		for(ReviewVO vo : list)
+//			System.out.println(vo);
+//	}
+//	
+//	@Test
+//	void findOrderInfoForReviewByOrderNoAndProductNo() {
+//		int orderNo = 1;
+//		int productNo = 332;
+//		
+//		Map<String, String> map = new HashMap<>();
+//		map.put("PRODUCT_NAME", reviewMapper.findProductNameForReviewByProductNo(productNo));
+//		map.put("ORDER_TIME", reviewMapper.findOrderTimeForReviewByOrderNo(orderNo));
+//		
+//		System.out.println(map.get("PRODUCT_NAME"));
+//		System.out.println(map.get("ORDER_TIME"));
+//	}
+//
+//	@Test
+//	void avgStarByProductNo() {
+//		int productNo = 331;
+//		int avgStar = 0;
+//		List<ReviewVO> list = reviewMapper.findReviewListByProductNo(productNo);
+//		if(list.size()!=0) {
+//			avgStar = reviewMapper.avgStarByProductNo(productNo);
+//		}
+//		System.out.println(avgStar);
+//	}
+//	
+//	@Test
+//	void countProductInCartById() {
+//		String id = "spring1";
+//		int totalCount = cartAndWishlistMapper.countCartById(id);
+//		System.out.println(totalCount);
+//	}
+//	
+//	@Test
+//	void getAllReviewList() {
+//		String id = "spring1";
+//		List<ReviewVO> list = reviewMapper.getAllReviewList(id);
+//		for(ReviewVO vo : list)
+//			System.out.println(vo);
+//	}
 }
 
--- juiceta-project/src/test/java/org/kosta/juicetaproject/Testpp.java
+++ juiceta-project/src/test/java/org/kosta/juicetaproject/Testpp.java
@@ -15,156 +15,156 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Testpp {
-	private final QuestionMapper questionMapper;
-	private final CartAndWishlistMapper cartAndWishlistMapper;
-	
-	@Autowired
-	public Testpp(QuestionMapper questionMapper , CartAndWishlistMapper cartAndWishlistMapper) {
-		super();
-		this.questionMapper = questionMapper;
-		this.cartAndWishlistMapper = cartAndWishlistMapper;
-	}
-	
-// 문의사항 등록
+//	private final QuestionMapper questionMapper;
+//	private final CartAndWishlistMapper cartAndWishlistMapper;
+//	
+//	@Autowired
+//	public Testpp(QuestionMapper questionMapper , CartAndWishlistMapper cartAndWishlistMapper) {
+//		super();
+//		this.questionMapper = questionMapper;
+//		this.cartAndWishlistMapper = cartAndWishlistMapper;
+//	}
+//	
+//// 문의사항 등록
+////	@Test
+////	public void registerQuestion() {
+////		MemberVO memberVO = new MemberVO("pjs","a",null,null,null,null);
+////		ProductVO productVO = new ProductVO(1,null,0,0,null,null,null,0);
+////		QuestionVO questionVO = new QuestionVO(1,"제목입니다2","답변입니다2","2022-12-16",0,memberVO,productVO);
+////		questionMapper.registerQuestion(questionVO);
+////	}
+//	// findQuestionAllListByProductNo(in productNo:int): List
+//	// 문의사항 목록 조회
+////	@Test
+////	public void findQuestionAllListByProductNo() {
+////		// findQuestionAllListByProductNo(in productNo:int): List
+////		// in productNo:int): List
+////		int productNo = 1;
+////		List<QuestionVO> list = questionMapper.findQuestionAllListByProductNo(productNo);
+////		System.out.println(list);
+////		for(int i = 0 ; i < list.size(); i++) {
+////			System.out.println(list.get(i));
+////		}
+////	}
+//	
+//	// 게시물 상세 조회
+//	@Test
+//	public void questionDetail() {
+//		int productNo = 1;
+//		QuestionVO questionVO= questionMapper.questionDetail(productNo);
+//		System.out.println(questionVO);
+//	}
+//	// 문의사항 등록
 //	@Test
 //	public void registerQuestion() {
-//		MemberVO memberVO = new MemberVO("pjs","a",null,null,null,null);
-//		ProductVO productVO = new ProductVO(1,null,0,0,null,null,null,0);
-//		QuestionVO questionVO = new QuestionVO(1,"제목입니다2","답변입니다2","2022-12-16",0,memberVO,productVO);
+//		//INSERT INTO juiceta_question(QUESTION_NO,QUESTION_TITLE,QUESTION_CONTENT,QUESTION_TIME , CHECK_ANSWER ,
+//		//PRODUCT_NO,id) VALUES(2,'제목입니다2','문의사항 입니다2','2022-12-18',0,64,'pjs');
+//		String id = "jtest";
+//		QuestionVO questionVO = new QuestionVO("제목입니다","문의사항입니다2",64,id);
 //		questionMapper.registerQuestion(questionVO);
 //	}
-	// findQuestionAllListByProductNo(in productNo:int): List
-	// 문의사항 목록 조회
+//		
+//	// 답변 수정
 //	@Test
-//	public void findQuestionAllListByProductNo() {
-//		// findQuestionAllListByProductNo(in productNo:int): List
-//		// in productNo:int): List
+//	public void updateQuestion3() {
 //		int productNo = 1;
-//		List<QuestionVO> list = questionMapper.findQuestionAllListByProductNo(productNo);
-//		System.out.println(list);
-//		for(int i = 0 ; i < list.size(); i++) {
-//			System.out.println(list.get(i));
+//		QuestionVO questionVO= questionMapper.questionDetail(productNo);
+//		System.out.println(questionVO);
+//	}
+//	
+//	// 답변 삭제 
+//	@Test
+//	public void deleteReview() {
+//		int productNo = 1;
+//		QuestionVO questionVO= questionMapper.questionDetail(productNo);
+//		System.out.println(questionVO);
+//	}
+//	
+//	// 답변 여부
+//	@Test
+//	public void findAnswerByQuestionNo() {
+//		int productNo = 1;
+//		AnswerVO answerVO = questionMapper.findAnswerByQuestionNo(productNo);
+//		System.out.println(answerVO);
+//	}
+//	
+//	// 답변이 달렸으면 답변여부 업데이트
+//	// UPDATE juiceta_question SET CHECK_ANSWER = 1 WHERE question_no = 109; 
+//	@Test
+//	public void updateQuestion() {
+//		int questionNo = 109;
+//		questionMapper.updateQuestion(questionNo);
+//	}
+//	
+//	// 장바구니 상세 목록 출력 리스트형식으로 
+//	// findCartAllListById(in id:String): List
+//	//	@Test
+//	//	public void findCartAllListById() {
+//	//		String id = "jtest3";
+//	//		List<ProductVO> list = cartAndWishlistMapper.findCartAllListById(id);
+//	//		for(int i = 0; i < list.size(); i++) {
+//	//			System.out.println(list.get(i));
+//	//		}
+//	//	}
+//	
+//	// SELECT * FROM juiceta_car 총 개수 구하는 메서드
+//	@Test
+//	public void countCartById() {
+//		String id="jtest3";
+//		int count = cartAndWishlistMapper.countCartById(id);
+//		System.out.println(count);
+//	}
+//	
+//	// 장바구니 삭제
+//	@Test
+//	public void deleteCartById() {
+//		int productNo = 64;
+//		cartAndWishlistMapper.deleteCartById(productNo);
+//	}
+//	
+//	@Test
+//	public void findAnswerByQuestionNo2() {
+//		int productNo = 1;
+//		AnswerVO answerVO = questionMapper.findAnswerByQuestionNo(productNo);
+//		System.out.println(answerVO);
+//	}
+//	
+//	// 장바구니 총 가격
+//	@Test
+//	public void totalCountPrice() {
+//		String id="jtest3";
+//		// 	int totalCountPrice(String id);
+//		int price = cartAndWishlistMapper.totalCountPrice(id);
+//		System.out.println(price);
+//	}
+//		
+//	// findQuestionCountByProductNo 5개 
+//	// getTotalProductCountbyCategory
+//	@Test
+//	public void findQuestionCountByProductNo() {
+//		int productNo1=332;
+//		int count = questionMapper.findQuestionCountByProductNo(productNo1);
+//		System.out.println(count);
+//	}
+//	//findQuestionCountByProductNo
+//	@Test
+//	public void findQuestionByRowNumber() {
+//		int productNo1 = 332;
+//		int TotalQuestionCount= questionMapper.findQuestionCountByProductNo(productNo1);
+//		String pageNo="";
+//		Pagination pagination = null;
+//		if(pageNo == "") 
+//			pagination = new Pagination(TotalQuestionCount);
+//		else 
+//			pagination = new Pagination(Integer.parseInt(pageNo), TotalQuestionCount);
+//		Map<String,Object> map = new HashMap<>();
+//
+//		map.put("PAGINATION",pagination);
+//		map.put("PRODUCT_NO", productNo1);	
+//		List<QuestionVO> list = questionMapper.findQuestionByRowNumber(map);	
+//		
+//		for(QuestionVO vo : list) {
+//			System.out.println(vo);
 //		}
 //	}
-	
-	// 게시물 상세 조회
-	@Test
-	public void questionDetail() {
-		int productNo = 1;
-		QuestionVO questionVO= questionMapper.questionDetail(productNo);
-		System.out.println(questionVO);
-	}
-	// 문의사항 등록
-	@Test
-	public void registerQuestion() {
-		//INSERT INTO juiceta_question(QUESTION_NO,QUESTION_TITLE,QUESTION_CONTENT,QUESTION_TIME , CHECK_ANSWER ,
-		//PRODUCT_NO,id) VALUES(2,'제목입니다2','문의사항 입니다2','2022-12-18',0,64,'pjs');
-		String id = "jtest";
-		QuestionVO questionVO = new QuestionVO("제목입니다","문의사항입니다2",64,id);
-		questionMapper.registerQuestion(questionVO);
-	}
-		
-	// 답변 수정
-	@Test
-	public void updateQuestion3() {
-		int productNo = 1;
-		QuestionVO questionVO= questionMapper.questionDetail(productNo);
-		System.out.println(questionVO);
-	}
-	
-	// 답변 삭제 
-	@Test
-	public void deleteReview() {
-		int productNo = 1;
-		QuestionVO questionVO= questionMapper.questionDetail(productNo);
-		System.out.println(questionVO);
-	}
-	
-	// 답변 여부
-	@Test
-	public void findAnswerByQuestionNo() {
-		int productNo = 1;
-		AnswerVO answerVO = questionMapper.findAnswerByQuestionNo(productNo);
-		System.out.println(answerVO);
-	}
-	
-	// 답변이 달렸으면 답변여부 업데이트
-	// UPDATE juiceta_question SET CHECK_ANSWER = 1 WHERE question_no = 109; 
-	@Test
-	public void updateQuestion() {
-		int questionNo = 109;
-		questionMapper.updateQuestion(questionNo);
-	}
-	
-	// 장바구니 상세 목록 출력 리스트형식으로 
-	// findCartAllListById(in id:String): List
-	//	@Test
-	//	public void findCartAllListById() {
-	//		String id = "jtest3";
-	//		List<ProductVO> list = cartAndWishlistMapper.findCartAllListById(id);
-	//		for(int i = 0; i < list.size(); i++) {
-	//			System.out.println(list.get(i));
-	//		}
-	//	}
-	
-	// SELECT * FROM juiceta_car 총 개수 구하는 메서드
-	@Test
-	public void countCartById() {
-		String id="jtest3";
-		int count = cartAndWishlistMapper.countCartById(id);
-		System.out.println(count);
-	}
-	
-	// 장바구니 삭제
-	@Test
-	public void deleteCartById() {
-		int productNo = 64;
-		cartAndWishlistMapper.deleteCartById(productNo);
-	}
-	
-	@Test
-	public void findAnswerByQuestionNo2() {
-		int productNo = 1;
-		AnswerVO answerVO = questionMapper.findAnswerByQuestionNo(productNo);
-		System.out.println(answerVO);
-	}
-	
-	// 장바구니 총 가격
-	@Test
-	public void totalCountPrice() {
-		String id="jtest3";
-		// 	int totalCountPrice(String id);
-		int price = cartAndWishlistMapper.totalCountPrice(id);
-		System.out.println(price);
-	}
-		
-	// findQuestionCountByProductNo 5개 
-	// getTotalProductCountbyCategory
-	@Test
-	public void findQuestionCountByProductNo() {
-		int productNo1=332;
-		int count = questionMapper.findQuestionCountByProductNo(productNo1);
-		System.out.println(count);
-	}
-	//findQuestionCountByProductNo
-	@Test
-	public void findQuestionByRowNumber() {
-		int productNo1 = 332;
-		int TotalQuestionCount= questionMapper.findQuestionCountByProductNo(productNo1);
-		String pageNo="";
-		Pagination pagination = null;
-		if(pageNo == "") 
-			pagination = new Pagination(TotalQuestionCount);
-		else 
-			pagination = new Pagination(Integer.parseInt(pageNo), TotalQuestionCount);
-		Map<String,Object> map = new HashMap<>();
-
-		map.put("PAGINATION",pagination);
-		map.put("PRODUCT_NO", productNo1);	
-		List<QuestionVO> list = questionMapper.findQuestionByRowNumber(map);	
-		
-		for(QuestionVO vo : list) {
-			System.out.println(vo);
-		}
-	}
 }
