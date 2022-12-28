027817b5d11f0d99c41202165cde02a21c38a158
ParkJinSoo189 <xnqlwlrh@naver.com>
2022-12-28 15:33:29 +0900

    박진수 테스트 주석 해제 , pom설정추가

--- juiceta-project/src/test/java/org/kosta/juicetaproject/Testlsh.java
+++ juiceta-project/src/test/java/org/kosta/juicetaproject/Testlsh.java
@@ -1,43 +1,46 @@
 package org.kosta.juicetaproject;
 
+import org.junit.jupiter.api.Test;
+import org.kosta.juicetaproject.model.mapper.BoardMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Testlsh {
-//	private final BoardMapper boardMapper;
-//	
-//	@Autowired
-//	public Testlsh(BoardMapper boardmapper) {
-//		super();
-//		this.boardMapper = boardmapper;
-//	}
-//	
-//	@Test
-//	void contextLoads() {
-//	}
-//	/*
-//	 * @Test public void boardlist() { List<BoardVO> list =
-//	 * boardMapper.findBoardAllList(); for(BoardVO vo : list)
-//	 * System.out.println(vo); }
-//	 * 
-//	 * @Test public void boardUpdate() { BoardVO vo = new BoardVO(1, "수정2", "수정2",
-//	 * null, 0, 0); boardMapper.boardUpdate(vo); List<BoardVO> list =
-//	 * boardMapper.findBoardAllList(); for(BoardVO bvo : list)
-//	 * System.out.println(bvo); }
-//	 * 
-//	 * @Test public void boardWrite() { String boardTitle = "test"; String
-//	 * boardContent = "text"; BoardVO vo= new
-//	 * BoardVO(0,boardTitle,boardContent,null,0, 0); boardMapper.registerBoard(vo);
-//	 * List<BoardVO> list = (List<BoardVO>) boardMapper.findBoardAllList(null);
-//	 * for(BoardVO bvo : list) System.out.println(bvo); }
-//	 * 
-//	 * @Test void deleteBoard() { int No = 1; boardMapper.deleteBoard(No);
-//	 * List<BoardVO> list = boardMapper.findBoardAllList(); for(BoardVO bvo : list)
-//	 * System.out.println(bvo); }
-//	 */
-//	
-//	@Test void countListBoard() {
-//		int countListBoard = boardMapper.getTotalBoardCount();
-//		System.out.println(countListBoard);
-//	}
+	private final BoardMapper boardMapper;
+	
+	@Autowired
+	public Testlsh(BoardMapper boardmapper) {
+		super();
+		this.boardMapper = boardmapper;
+	}
+	
+	@Test
+	void contextLoads() {
+	}
+	/*
+	 * @Test public void boardlist() { List<BoardVO> list =
+	 * boardMapper.findBoardAllList(); for(BoardVO vo : list)
+	 * System.out.println(vo); }
+	 * 
+	 * @Test public void boardUpdate() { BoardVO vo = new BoardVO(1, "수정2", "수정2",
+	 * null, 0, 0); boardMapper.boardUpdate(vo); List<BoardVO> list =
+	 * boardMapper.findBoardAllList(); for(BoardVO bvo : list)
+	 * System.out.println(bvo); }
+	 * 
+	 * @Test public void boardWrite() { String boardTitle = "test"; String
+	 * boardContent = "text"; BoardVO vo= new
+	 * BoardVO(0,boardTitle,boardContent,null,0, 0); boardMapper.registerBoard(vo);
+	 * List<BoardVO> list = (List<BoardVO>) boardMapper.findBoardAllList(null);
+	 * for(BoardVO bvo : list) System.out.println(bvo); }
+	 * 
+	 * @Test void deleteBoard() { int No = 1; boardMapper.deleteBoard(No);
+	 * List<BoardVO> list = boardMapper.findBoardAllList(); for(BoardVO bvo : list)
+	 * System.out.println(bvo); }
+	 */
+	
+	@Test void countListBoard() {
+		int countListBoard = boardMapper.getTotalBoardCount();
+		System.out.println(countListBoard);
+	}
 }
--- juiceta-project/src/test/java/org/kosta/juicetaproject/Testpjs.java
+++ juiceta-project/src/test/java/org/kosta/juicetaproject/Testpjs.java
@@ -1,126 +1,140 @@
 package org.kosta.juicetaproject;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.kosta.juicetaproject.model.mapper.OrderMapper;
+import org.kosta.juicetaproject.model.mapper.ProductMapper;
+import org.kosta.juicetaproject.model.mapper.ReviewMapper;
+import org.kosta.juicetaproject.model.vo.MemberVO;
+import org.kosta.juicetaproject.model.vo.OrderDetailVO;
+import org.kosta.juicetaproject.model.vo.OrderVO;
+import org.kosta.juicetaproject.model.vo.Pagination;
+import org.kosta.juicetaproject.model.vo.ProductVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 
 class Testpjs {
-//	private final ProductMapper productMapper;
-//	private final OrderMapper orderMapper;
-//	private final ReviewMapper reviewMapper;
-//	@Autowired
-//	public Testpjs(ProductMapper productMapper,OrderMapper orderMapper,ReviewMapper reviewMapper) {
-//		super();
-//		this.productMapper = productMapper;
-//		this.orderMapper= orderMapper;
-//		this.reviewMapper=reviewMapper;
-//	}
-//	@Test
-//	void productDetail() {
-//		int productNo=1;
-//		ProductVO productVO=productMapper.findProductByProductNo(productNo);
-//		System.out.println(productVO);
-//	}
-//
-//	/*
-//	 * @Test void updateProduct() { int productNo=6; ProductVO productVO=new
-//	 * ProductVO(productNo, "브로콜리즙", 14000, 30, "맛있는 브로콜리즙", "product-6.jpg",
-//	 * "과일즙"); int result=productMapper.updateProduct(productVO);
-//	 * System.out.println(result); }
-//	 * 
-//	 * @Test void registerProduct() { ProductVO productVO=new ProductVO(11, "test2",
-//	 * 5, 5, "test2", "product-11.jpg", "test3"); int
-//	 * result=productMapper.registerProduct(productVO); System.out.println(result);
-//	 * }
-//	 */
-//	@Test
-//	void deleteProduct() {
-//		int productNo=49;
-//		int result=productMapper.deleteProduct(productNo);
-//		System.out.println(result);
-//	}
-//
-//	@Test
-//	void findProductListByKeyword() {
-//		int totalProductCount =productMapper.getTotalProductCount();
-//		String pageNo="";
-//		Pagination pagination = null;
-//		
-//		if(pageNo=="")
-//			pagination = new Pagination(totalProductCount);
-//		else {
-//			pagination = new Pagination(Integer.parseInt(pageNo), totalProductCount);
-//		}
-//		String keyword = "파"; 
-//		Map<String, Object> map = new HashMap<>();
-//		map.put("PAGINATION", pagination);
-//		map.put("KEYWORD",keyword);
-//		
-//		List<ProductVO> list = productMapper.findProductListByKeyword(map);
-//		for(ProductVO vo : list)
-//			System.out.println(vo);
-//	}
-//	@Test
-//	void productAllListByRnum() {
-//		int totalProductCount = productMapper.getTotalProductCount(); // 총상품수를 구하는 메서드
-//		String pageNo = ""; //페이지번호 변수
-//		Pagination pagination = null; //페이지네이션사용을위해 선언
-//		
-//		if(pageNo=="") //만약 페이지번호가 없을 때 
-//			pagination = new Pagination(totalProductCount); //페이지네이션 변수에 총상품수를 매개변수로 담아준다
-//		else // 페이지번호가 있을 때
-//			pagination = new Pagination(Integer.parseInt(pageNo), totalProductCount);
-//			// 페이지네이션 변수에 페이지번호와 총상품수를 매개변수에 넣어준다
-//
-//		List<ProductVO> list = productMapper.findAllProduct(pagination); 
-//		//ProductVO 타입만 들어올 수 있는 리스트에 페이지네이션을  
-//		// 전체상품리스트를 구하는 메서드에 매개변수로 넣어준다
-//		for(ProductVO vo : list) // list.size만큼 반복문을 돌려줘서 
-//			System.out.println(vo);// vo 값들을 출력한다
-//	}
-//	@Test
-//	void findOrderById() {
-//		MemberVO memberVO=new MemberVO();
-//		memberVO.setId("jtest3");
-//		OrderDetailVO OrderDetailVO=orderMapper.findOrderById(memberVO.getId());
-//		System.out.println(OrderDetailVO);
-//	}
-//	@Test
-//	void findCountProductByKeyword() {
-//		String keyword="양배추";
-//		int result=productMapper.findCountProductByKeyword(keyword);
-//		System.out.println(result);
-//	}
-//	//주문번호에 대해 주문번호 주문날짜 배송주소를 출력
-//	@Test
-//	void findOrderInfoByOrderNo() {
-//		int orderNo=43;
-//		OrderVO orderVO=orderMapper.findOrderInfoByOrderNo(orderNo);
-//		System.out.println(orderVO);
-//	}
-//	//주문번호를 통해 상품명,주문수량,총합계를 출력
-//	@Test
-//	void findOrderTotalPriceInfoByOrderNo() {
-//		int orderNo=1;
-//		List<Map<String, Object>> list=orderMapper.findOrderTotalPriceInfoByOrderNo(orderNo);
-//		System.out.println(list);
-//	}
-//	//아이디,주문번호,제품번호로 작성한 리뷰 갯수 출력
-//	@Test
-//	void findTotalCountReview() {
-//		String id="java189";
-//		int orderNo=4;
-//		int productNo=332;
-//		int count=orderMapper.findTotalCountReview(id,orderNo,productNo);
-//		System.out.println(count);
-//	}
-//	@Test
-//	void findCountByCheckReview() {
-//		int orderNo=5;
-//		int productNo=326;
-//		int count=reviewMapper.findCountByCheckReview(orderNo,productNo);
-//		System.out.println(count);
-//	}
+	private final ProductMapper productMapper;
+	private final OrderMapper orderMapper;
+	private final ReviewMapper reviewMapper;
+	@Autowired
+	public Testpjs(ProductMapper productMapper,OrderMapper orderMapper,ReviewMapper reviewMapper) {
+		super();
+		this.productMapper = productMapper;
+		this.orderMapper= orderMapper;
+		this.reviewMapper=reviewMapper;
+	}
+	@Test
+	void productDetail() {
+		int productNo=1;
+		ProductVO productVO=productMapper.findProductByProductNo(productNo);
+		System.out.println(productVO);
+	}
+
+	/*
+	 * @Test void updateProduct() { int productNo=6; ProductVO productVO=new
+	 * ProductVO(productNo, "브로콜리즙", 14000, 30, "맛있는 브로콜리즙", "product-6.jpg",
+	 * "과일즙"); int result=productMapper.updateProduct(productVO);
+	 * System.out.println(result); }
+	 * 
+	 * @Test void registerProduct() { ProductVO productVO=new ProductVO(11, "test2",
+	 * 5, 5, "test2", "product-11.jpg", "test3"); int
+	 * result=productMapper.registerProduct(productVO); System.out.println(result);
+	 * }
+	 */
+	@Test
+	void deleteProduct() {
+		int productNo=49;
+		int result=productMapper.deleteProduct(productNo);
+		System.out.println(result);
+	}
+
+	@Test
+	void findProductListByKeyword() {
+		int totalProductCount =productMapper.getTotalProductCount();
+		String pageNo="";
+		Pagination pagination = null;
+		
+		if(pageNo=="")
+			pagination = new Pagination(totalProductCount);
+		else {
+			pagination = new Pagination(Integer.parseInt(pageNo), totalProductCount);
+		}
+		String keyword = "파"; 
+		Map<String, Object> map = new HashMap<>();
+		map.put("PAGINATION", pagination);
+		map.put("KEYWORD",keyword);
+		
+		List<ProductVO> list = productMapper.findProductListByKeyword(map);
+		for(ProductVO vo : list)
+			System.out.println(vo);
+	}
+	@Test
+	void productAllListByRnum() {
+		int totalProductCount = productMapper.getTotalProductCount();  //총상품수를 구하는 메서드
+		String pageNo = ""; //페이지번호 변수
+		Pagination pagination = null; //페이지네이션사용을위해 선언
+		
+		if(pageNo=="") //만약 페이지번호가 없을 때 
+			pagination = new Pagination(totalProductCount); //페이지네이션 변수에 총상품수를 매개변수로 담아준다
+		else  //페이지번호가 있을 때
+			pagination = new Pagination(Integer.parseInt(pageNo), totalProductCount);
+			 //페이지네이션 변수에 페이지번호와 총상품수를 매개변수에 넣어준다
+
+		List<ProductVO> list = productMapper.findAllProduct(pagination); 
+		//ProductVO 타입만 들어올 수 있는 리스트에 페이지네이션을  
+		 //전체상품리스트를 구하는 메서드에 매개변수로 넣어준다
+		for(ProductVO vo : list)  //list.size만큼 반복문을 돌려줘서 
+			System.out.println(vo); //vo 값들을 출력한다
+	}
+	@Test
+	void findOrderById() {
+		MemberVO memberVO=new MemberVO();
+		memberVO.setId("jtest3");
+		OrderDetailVO OrderDetailVO=orderMapper.findOrderById(memberVO.getId());
+		System.out.println(OrderDetailVO);
+	}
+	@Test
+	void findCountProductByKeyword() {
+		String keyword="양배추";
+		int result=productMapper.findCountProductByKeyword(keyword);
+		System.out.println(result);
+	}
+	//주문번호에 대해 주문번호 주문날짜 배송주소를 출력
+	@Test
+	void findOrderInfoByOrderNo() {
+		int orderNo=43;
+		OrderVO orderVO=orderMapper.findOrderInfoByOrderNo(orderNo);
+		System.out.println(orderVO);
+	}
+	//주문번호를 통해 상품명,주문수량,총합계를 출력
+	@Test
+	void findOrderTotalPriceInfoByOrderNo() {
+		int orderNo=1;
+		List<Map<String, Object>> list=orderMapper.findOrderTotalPriceInfoByOrderNo(orderNo);
+		System.out.println(list);
+	}
+	//아이디,주문번호,제품번호로 작성한 리뷰 갯수 출력
+	@Test
+	void findTotalCountReview() {
+		String id="java189";
+		int orderNo=4;
+		int productNo=332;
+		int count=orderMapper.findTotalCountReview(id,orderNo,productNo);
+		System.out.println(count);
+	}
+	@Test
+	void findCountByCheckReview() {
+		int orderNo=5;
+		int productNo=326;
+		int count=reviewMapper.findCountByCheckReview(orderNo,productNo);
+		System.out.println(count);
+	}
 }
 
 
